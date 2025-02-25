5015d0def215b9e81f7266916bf5eebdef0605f5
VeljkoRikalovic <velja001@yahoo.com>
2025-02-25 13:03:48 +0100

    api update

--- app/_lib/data-service.js
+++ app/_lib/data-service.js
@@ -37,7 +37,7 @@ export async function getCabinPrice(id) {
   return data;
 }
 
-export const getCabins = async function () {
+export async function getCabins() {
   const { data, error } = await supabase
     .from("cabins")
     .select("id, name, maxCapacity, regularPrice, discount, image")
@@ -49,7 +49,7 @@ export const getCabins = async function () {
   }
 
   return data;
-};
+}
 
 // Guests are uniquely identified by their email address
 export async function getGuest(email) {
